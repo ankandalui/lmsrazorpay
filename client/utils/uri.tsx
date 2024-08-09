7a671bab7d22a33e0b5d548c7bfbb77377643001
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-09 21:47:04 +0530

    changes sexy

--- client/utils/uri.tsx
+++ client/utils/uri.tsx
@@ -1,3 +1,3 @@
-export const SERVER_URI ="http://192.168.0.107:8081/api/v1"
-process.env.SERVER_URI || "http://192.168.0.107:8081/api/v1";
+export const SERVER_URI ="http://192.168.0.107:8000/api/v1"
+process.env.SERVER_URI || "http://192.168.0.107:8000/api/v1";
 
