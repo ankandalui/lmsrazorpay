75da701c0306beac591fd927c5ec5b065675ca09
sandipan-das-sd <sd901656@gmail.com>
2024-08-10 00:51:44 +0530

    added

--- client/utils/uri.tsx
+++ client/utils/uri.tsx
@@ -1,5 +1,5 @@
-export const SERVER_URI ="http://192.168.0.107:8000/api/v1"
-process.env.SERVER_URI || "http://192.168.0.107:8000/api/v1";
+export const SERVER_URI ="http://192.168.29.88:8000/api/v1"
+process.env.SERVER_URI || "http://192.168.29.88:8000/api/v1";
 
 
 
