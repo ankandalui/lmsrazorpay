83842d2b30120c65efa356bfd8b45f8ab0fb7142
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-08-11 20:03:49 +0530

    done

--- client/utils/uri.tsx
+++ client/utils/uri.tsx
@@ -1,3 +1,4 @@
-export const SERVER_URI ="http://192.168.29.88:8000/api/v1"
-process.env.SERVER_URI || "http://192.168.29.88:8000/api/v1";
+export const SERVER_URI ="http://192.168.0.105:8000/api/v1"
+process.env.SERVER_URI || "http://192.168.0.105:8000/api/v1";
+
 
